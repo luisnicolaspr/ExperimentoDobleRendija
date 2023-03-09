ddb89413c38b18f92b767a5d4b877dbdda560d66
luisnicolaspr <124225445+luisnicolaspr@users.noreply.github.com>
2023-03-09 12:29:30 -0500

    Add files via upload

---
+++ classicaltoquantum.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plot
+import numpy as np
+
+from  matrixAndVectorLibrary import *
+
+def finalMatrix( matrix ):
+    row, column = len( matrix ), len( matrix[0] )
+    for i in range( row ):
+        nRow = []
+        for j in range( column ):
+            nRow.append( [( module( matrix[ i ][ j ] )**2 ),0])
+            
+        matrix[ i ]= nRow
+    return matrix
+
+def quantumProbabilisticSystem( matrix, vectIni, clicks ):
+    if ( clicks  >= 0 ) and ( type( clicks ) is int ):
+        length  = len( vectIni )
+        copyMatrix = matrix[:]
+        
+        for x in range(clicks  ):
+            matrix = multiplicaMat( matrix, copyMatrix)
+
+        
+        return finalMatrix( matrix )
+    return -1
+
+def multipleSlitQuantumExperiment( matrix , vectIni, clicks  ):
+        return quantumProbabilisticSystem( matrix , vectIni, clicks  )
+
+    
+def graphProbabilitiesVector( vector  ):
+    data = len( vector )
+    x = np.array([ x for x in range( data )])
+    y = np.array([ round(vector[x][0]*100,2) for x in range( data )])
+
+    plot.bar( x,y , color ='g', align='center')
+    plot.title('Probabilidades vector')
+    plot.show()
